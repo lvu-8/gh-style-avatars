80b59524172f19d212f43ee8e2668539732b81e8
Lubomyr voloshynovsky <lvu8@proton.me>
2025-08-23 13:12:56 +0000

    refactored and optimizated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use image::{Rgb, RgbImage};
 use md5::{Digest, Md5};
-use std::str::Chars;
 
 const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
 
@@ -17,11 +16,9 @@ impl AvatarGenerator {
     pub fn generate_avatar(&mut self, input: &str) -> RgbImage {
         let hash = self.hash_input(input);
         let color = self.extract_color(&hash);
-        let bin = self.get_bin_by_hash(hash.clone());
-        let mut chars = bin.chars();
 
         let mut img = RgbImage::from_pixel(SIZE, SIZE, color);
-        self.fill_avatar(&mut img, &mut chars);
+        self.fill_avatar(&mut img, &hash);
 
         img
     }
@@ -30,35 +27,42 @@ impl AvatarGenerator {
         let length = hash.len();
         let mut color = Rgb([0, 0, 0]);
 
-        for i in 1..4 {
-            color.0[i - 1] = hash[length - i];
+        for i in 0..3 {
+            color.0[i] = hash[length - i - 1];
         }
 
         color
     }
 
-    fn fill_avatar(&self, img: &mut RgbImage, chars: &mut Chars<'_>) {
+    fn fill_avatar(&self, img: &mut RgbImage, hash: &[u8]) {
+        let mut bit_index = 0;
+
         for y in 0..SIZE {
             for x in 0..CENTER {
-                if chars.next() == Some('1') {
+                if self.bit_at(hash, bit_index) == 1 {
                     img.put_pixel(x, y, WHITE);
                     img.put_pixel(SIZE - x - 1, y, WHITE);
                 }
+                bit_index += 1;
             }
-            if chars.next() == Some('1') {
+            if self.bit_at(hash, bit_index) == 1 {
                 img.put_pixel(CENTER, y, WHITE);
             }
+            bit_index += 1;
         }
+    }
+
+    fn bit_at(&self, hash: &[u8], i: usize) -> u8 {
+        let byte = hash[i / 8];
+        
+        let bit = 7 - (i % 8);
+        (byte >> bit) & 1
     }
 
     fn hash_input(&self, input: &str) -> Vec<u8> {
         let mut hasher = Md5::new();
         hasher.update(input.as_bytes());
         hasher.finalize().to_vec()
-    }
-
-    fn get_bin_by_hash(&self, hash: Vec<u8>) -> String {
-        hash.iter().map(|byte| format!("{:08b}", byte)).collect()
     }
 }
 
