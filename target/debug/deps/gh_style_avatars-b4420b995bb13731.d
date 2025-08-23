eee38f67c9b2de3e4441343d58bb3a11fed6f7fb
User <lvu8@proton.me>
2025-08-23 11:11:35 +0000

    first commit

---
+++ target/debug/deps/gh_style_avatars-b4420b995bb13731.d
@@ -0,0 +1,5 @@
+/home/user/programing/projects/rust/interesting/gh-style-avatars/target/debug/deps/libgh_style_avatars-b4420b995bb13731.rmeta: src/main.rs
+
+/home/user/programing/projects/rust/interesting/gh-style-avatars/target/debug/deps/gh_style_avatars-b4420b995bb13731.d: src/main.rs
+
+src/main.rs:
